8fa86f9e41c128fc6fd13c3373c37e37bc1193d7
Paul Maloney <paul@paulmaloney.com>
2024-10-16 07:21:17 -0700

    completed

--- frontend/components/Quotes.js
+++ frontend/components/Quotes.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { QuotesContext } from "../context/quotesContext";
 
 export default function Quotes(props) {
   const {
@@ -9,38 +10,43 @@ export default function Quotes(props) {
     editQuoteAuthenticity,
     setHighlightedQuote,
     toggleVisibility,
-  } = props
+  } = useContext(QuotesContext);
+
   return (
     <div id="quotes">
       <h3>Quotes</h3>
       <div>
-        {
-          quotes
-            ?.filter(qt => {
-              return displayAllQuotes || !qt.apocryphal
-            })
-            .map(qt => (
-              <div
-                key={qt.id}
-                className={`quote${qt.apocryphal ? " fake" : ''}${highlightedQuote === qt.id ? " highlight" : ''}`}
-              >
-                <div>{qt.quoteText}</div>
-                <div>{qt.authorName}</div>
-                <div className="quote-buttons">
-                  <button onClick={() => deleteQuote(qt.id)}>DELETE</button>
-                  <button onClick={() => setHighlightedQuote(qt.id)}>HIGHLIGHT</button>
-                  <button onClick={() => editQuoteAuthenticity(qt.id)}>FAKE</button>
-                </div>
+        {quotes
+          ?.filter((qt) => {
+            return displayAllQuotes || !qt.apocryphal;
+          })
+          .map((qt) => (
+            <div
+              key={qt.id}
+              className={`quote${qt.apocryphal ? " fake" : ""}${
+                highlightedQuote === qt.id ? " highlight" : ""
+              }`}
+            >
+              <div>{qt.quoteText}</div>
+              <div>{qt.authorName}</div>
+              <div className="quote-buttons">
+                <button onClick={() => deleteQuote(qt.id)}>DELETE</button>
+                <button onClick={() => setHighlightedQuote(qt.id)}>
+                  HIGHLIGHT
+                </button>
+                <button onClick={() => editQuoteAuthenticity(qt.id)}>
+                  FAKE
+                </button>
               </div>
-            ))
-        }
-        {
-          !quotes?.length && "No quotes here! Go write some."
-        }
+            </div>
+          ))}
+        {!quotes?.length && "No quotes here! Go write some."}
       </div>
-      {!!quotes?.length && <button onClick={toggleVisibility}>
-        {displayAllQuotes ? 'HIDE' : 'SHOW'} FAKE QUOTES
-      </button>}
+      {!!quotes?.length && (
+        <button onClick={toggleVisibility}>
+          {displayAllQuotes ? "HIDE" : "SHOW"} FAKE QUOTES
+        </button>
+      )}
     </div>
-  )
+  );
 }
--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,15 @@
 import './styles/reset.css'
 import './styles/styles.css'
-import React from 'react'
+import React, {createContext} from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App'
+import { QuotesProvider } from './context/quotesContext'
 
 const domNode = document.getElementById('root')
 const root = createRoot(domNode)
 
 root.render(
-  <App />
+  <QuotesProvider>
+    <App />
+  </QuotesProvider>
 )
